eb2d7587d411a3592bf8040261c7d6dfd4bde5b8
Gianmarco Lugaresi <118520779+GianmarcoLugaresi@users.noreply.github.com>
2025-11-04 18:55:34 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: '/Somnium/'
+    base: '/Somnium/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
